4d864bce5f0b9639539476c57dcfe3efcfa68b3f
Kameron Heyen <kheyen@dontpaniclabs.com>
2024-11-10 23:08:01 -0600

    Make front end test

--- nba-tracker-app/src/app/components/player-stats-table/player-stats-table.component.spec.ts
+++ nba-tracker-app/src/app/components/player-stats-table/player-stats-table.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PlayerStatsTableComponent } from './player-stats-table.component';
+import {BoxScore} from "../../DataContracts/BoxScore";
 
 describe('PlayerStatsTableComponent', () => {
   let component: PlayerStatsTableComponent;
@@ -14,10 +15,39 @@ describe('PlayerStatsTableComponent', () => {
 
     fixture = TestBed.createComponent(PlayerStatsTableComponent);
     component = fixture.componentInstance;
+
+    const boxScore: BoxScore = {
+      gameId: "1",
+      gameTime: new Date(),
+      homeTeam: {
+        teamId: 1,
+        teamName: "Home",
+        wins: 0,
+        losses: 0,
+        teamTricode: "HOM",
+        teamCity: "Home",
+        score: 0,
+        timeoutsRemaining: 0
+      },
+      awayTeam: {
+        teamId: 2,
+        teamName: "Away",
+        wins: 0,
+        losses: 0,
+        teamTricode: "AWY",
+        teamCity: "Away",
+        score: 0,
+        timeoutsRemaining: 0
+      },
+      homeTeamPlayers: [],
+      awayTeamPlayers: []
+    };
+    component.boxScore = boxScore;
     fixture.detectChanges();
   });
 
   it('should create', () => {
+
     expect(component).toBeTruthy();
   });
 });
