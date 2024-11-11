1de2219e5103bbae4332b3ebb068d31b14e84359
Kameron Heyen <kheyen@dontpaniclabs.com>
2024-11-10 22:56:47 -0600

    Update App routes

--- nba-tracker-app/src/app/app.routes.ts
+++ nba-tracker-app/src/app/app.routes.ts
@@ -3,6 +3,11 @@ import {ScorePageComponent} from "./pages/score-page/score-page.component";
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: '/scores',
+    pathMatch: 'full'
+  },
+  {
     path: 'scores',
     component: ScorePageComponent
   }
